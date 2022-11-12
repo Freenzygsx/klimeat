420b53a1ea6b9f47032a1dc906f20a6eac9f9864
Freenzygsx <ezziel.trabajo@gmail.com>
2022-11-11 21:19:02 -0400

    Config weather app and make api call to get weather

--- weather/urls.py
+++ weather/urls.py
@@ -3,4 +3,5 @@ from weather import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('weather/<str:city>', views.getWeather, name='weather')
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from decouple import config
+
+
 
 def index(request):
     return render(request, 'index.html')
+
+def getWeather(request, city):
+    rlatlon = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={config('api_key')}")
+    rlatlonjson = rlatlon.json()[0]
+    lat = rlatlonjson['lat']
+    lon = rlatlonjson['lon']
+    rweather = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={config('api_key')}")
+    rjweather = rweather.json()
+    print(rjweather['list'])
+
+    return HttpResponse(rlatlon)
